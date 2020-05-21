1d60e45a69b50f784899963a86e0812ff7a2f9ae
Zmagis <eglegustaite@gmail.com>
2020-05-21 14:31:55 +0300

    connect node with react

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const cors = require('cors');
+var mysql = require('mysql');
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// CORS niddleware, enable CORS on the server
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+    app.use(allowCrossDomain);
+
+
+
+
+// Create connection to database
+var db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'sys',
+  insecureAuth : true
+});
+
+// Connect to database
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+
+
+
+// route for showing products in client index page
+app.get('/api/product', (req, res)=> {
+    //const product  = [
+        //{ id: 1, name: 'Tv', price: 10 },
+        //{ id: 2, name: 'PC', price: 11 },
+       // { id: 3, name: 'batas', price: 12 },
+   // ];
+   let sql = 'SELECT * FROM products'; // Selecting products from already created table on mySQL workbench
+   let query = db.query(sql,(err, product) => {
+    if (err) throw err;
+    console.log(product);
+    res.json(product)
+   });
+    //res.json(product);
+});
+
+
+// get route from client submited data into login form
+
+app.post('/create',(req, res) =>{
+    const userInput = {
+        username: req.body.username.value,
+        userpass: req.body.password.value
+    };
+    console.log(userInput);
+    let sql = 'INSERT INTO users SET ?';
+    let query = db.query(sql, userInput, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+    });
+
+    res.status(201).json({"some":"connected"})
+});
+
+
+const port = 9000;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
---
+++ shop-master/src/components/home/Home.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import box from "../../img/box.jpg";
+
+const Home = () => {
+  // let products = [
+  //   { title: "One", price: "453", img: box },
+  //   { title: "Two", price: "453", img: box },
+  //   { title: "Three", price: "453", img: box },
+  //   { title: "Four", price: "453", img: box },
+  //   { title: "Five", price: "453", img: box },
+  //   { title: "Six", price: "453", img: box },
+  //   { title: "Seven", price: "453", img: box },
+  //   { title: "Eight", price: "453", img: box },
+  // ];
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((products) => {
+        setProducts([...products]);
+        console.log(products);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <div className="container">
+        {products.map((product) => (
+          <div className="product">
+            <img src={box} alt="" />
+            <p>{product.idProducts}</p>
+            <p>{product.Name}</p>
+            <p>Price {product.Price}</p>
+            <button key={product.idProducts}> Add to basket</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
