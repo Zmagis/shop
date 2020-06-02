1b9b23d0f979b1d2bc13daaaacdfea437d9728f8
mgrae <canyon767@gmail.com>
2020-06-02 12:53:28 +0300

    added payment route to ProductDetails

--- shop-master/src/components/home/ProductDetails/ProductDetails.js
+++ shop-master/src/components/home/ProductDetails/ProductDetails.js
@@ -1,7 +1,27 @@
 import React from "react";
 import "./ProductDetails.css";
+import axios from "axios";
+import StripeCheckout from "react-stripe-checkout";
+
 
 const ProductDetails = ({ data, children }) => {
+  console.log(data.Price);
+  const makePayment = (token) => {
+    const body = {
+      token,
+      total: data.Price,
+      ids: data.idProducts
+      //total,
+    };
+    axios.post("/payment", body).then((result) => {
+      if (result.status === 200) {
+        alert("success");
+      } else {
+        alert("error");
+      }
+    });
+  };
+
   return (
     <div className="product-details">
       <img src={data.image} alt="" />
@@ -18,7 +38,17 @@ const ProductDetails = ({ data, children }) => {
             <p>Views: {data.views}</p>
           </div>
           <div style={{ display: "flex", alignItems: "flex-end" }}>
-            {children}
+          {children[0]}
+            <StripeCheckout
+          stripeKey="pk_test_OtPH56R0K9McMN5SdhabDEKC"
+          token={makePayment}
+          name={data.Name}
+          amount={data.Price * 100}
+          shippingAddress
+          billingAddress
+        >
+          {children[1]}
+        </StripeCheckout>
           </div>
         </div>
       </div>
